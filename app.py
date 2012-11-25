249dacd94e66a2b4ff8ba203f8e966100c484e78
Michael Ballantyne <michael.ballantyne@gmail.com>
2012-11-24 19:00:26 -0700

    modify for wsgi compat

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-from slstracker import app
+from slstracker import app as application
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0', debug=True)
