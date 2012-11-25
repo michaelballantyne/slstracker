e52daa6724a1248798107b6258052c96aded1df1
Michael Ballantyne <michael.ballantyne@gmail.com>
2012-11-24 18:03:17 -0700

    change schema to create if not exists

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
-create table semester (
+create table if not exists semester (
     id serial primary key,
     name varchar(50) unique not null,
     active boolean not null
 );
 
-create table final_project (
+create table if not exists final_project (
     id serial primary key,
     body bytea not null
 ); 
 
-create table student (
+create table if not exists student (
     id serial primary key,
     username varchar(50) unique not null,
     name varchar(50) not null,
@@ -17,21 +17,21 @@ create table student (
     final_project int references final_project(id)
 );
 
-create table student_semester (
+create table if not exists student_semester (
     semester int references semester(id),
     student int references student(id),
     reflection text,
     primary key (semester, student)
 );
 
-create table organization (
+create table if not exists organization (
     id serial primary key,
     name varchar(50) unique not null,
     contact_name varchar(50) not null,
     contact_phone varchar(50) not null
 );
 
-create table hours_entry (
+create table if not exists hours_entry (
     id serial primary key,
     student int not null,
     semester int not null,
