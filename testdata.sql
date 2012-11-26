ff3b0f2ef6ef3dd46f70434410862259dc897c80
Michael Ballantyne <michael.ballantyne@gmail.com>
2012-11-25 19:36:02 -0700

    remove some unneeded files from my dev env

--- testdata.sql
+++
@@ -1,9 +0,0 @@
-insert into student (username, name) values ('mjb0321', 'Michael Ballantyne'),('djt0231', 'Daniel Tanner'),('zym0210', 'Zachary Morin');
-
-insert into semester (name,active) values ('Fall 2010','false'),('Spring 2011','true'),('Fall 2011','true'),('Spring 2012','false');
-
-insert into student_semester (student, semester) values (1,1),(1,2),(2,3);
-
-insert into organization (name, contact_name, contact_phone) values ('BSA', 'Bill Milner', '801-483-1444');
-
-insert into hours_entry (student, semester, event_date, hours, organization, activity) values (1,1,'Oct-08-2011',5,1, 'service project'),(1,1,'Oct-23-2011',3,1, 'meeting');
