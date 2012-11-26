cfb490f08134be7d013db0dceaf0998eeb4bc81c
Michael Ballantyne <michael.ballantyne@gmail.com>
2012-11-25 19:32:46 -0700

    remove local emptydb script

--- emptydb.sh
+++
@@ -1,5 +0,0 @@
-psql -d postgres -c "drop database slstracker;"
-psql -d postgres -c "create database slstracker;"
-psql -d postgres -c "alter database slstracker owner to slstracker;"
-psql -d slstracker -U slstracker -f schema.sql
-psql -d slstracker -U slstracker -f testdata.sql
